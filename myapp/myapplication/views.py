40dd48204e6c216a62d465ae0a52b68628d52d29
charmainemimie <charmainemangorima@gmail.com>
2025-02-04 21:49:16 +0100

    home page setup

--- myapp/myapplication/views.py
+++ myapp/myapplication/views.py
@@ -2,4 +2,4 @@ from django.shortcuts import render
 from django.http import HttpResponse
 # Create your views here.
 def index(request):
-    return  HttpResponse('<h1>Hello</h1>')
+    return  HttpResponse('<h1>Hello, welcome to my site</h1>')
